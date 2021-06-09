95292ab9d48423802c24c1ff3375a7bbe5d4a959
SeonIN <tjsdls0724@naver.com>
2021-06-09 23:11:53 +0900

    백엔드 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -18,6 +18,7 @@
 /* express 사용 */
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
@@ -30,6 +31,9 @@ app.set('views', './src/views');
 app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
 
 module.exports = app;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,51 @@
 "use strict";
 
-const home = (req,res) => {
-    res.render("home/index");
+const output = {
+    home : (req,res) => {
+        res.render("home/index");
+    },
+    login : (req,res) => {
+        res.render("home/login");
+    },
+};
+
+const users = {
+    id : ["kimseonin","seonin","sunin"],
+    psword : ["1234","1234","12345"],
 }
 
-const login = (req,res) => {
-    res.render("home/login");
-}
+const process = {
+    login : (req,res) => {
+        const id = req.body.id;
+        const psword = req.body.psword;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success : true,
+                })
+            }
+        }
+        return res.json({
+            success : false,
+            msg:"로그인에 실패 하셨습니다",
+        })
+    },
+};
+
+
+// const home = (req,res) => {
+//     res.render("home/index");
+// }
+
+// const login = (req,res) => {
+//     res.render("home/login");
+// }
 
 module.exports = {
-    home,
-    login
+    // home,
+    // login
+    output,
+    process
 }
